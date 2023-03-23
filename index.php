b25c5f4e639b305135f06d431ce0bb6b9cc75e9b
vishalupadhyayrsm <vishalm.rsm@gmail.com>
2023-03-23 09:24:27 +0530

    data

--- index.php
+++ index.php
@@ -5,7 +5,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
+<body class="bg-slate-800">
+    <div>
+        <h2 class="text-center font-serif text-base font-black text-white">Face Rec</h2>
+    </div>
+    <div class="">
+        <div class="">Video COnatiner</div>
+        <div class="">Output COntainer</div>
+    </div>
   <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
