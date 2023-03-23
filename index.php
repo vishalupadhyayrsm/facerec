ce3bda0602b24042ed1ce98a39e93cdfedb23742
vishalupadhyayrsm <vishalm.rsm@gmail.com>
2023-03-23 09:45:07 +0530

    data

--- index.php
+++ index.php
@@ -11,17 +11,9 @@
     </div>
    
     <div class="grid grid-rows grid-flow-col gap-4">
-        <div class="row-span-2 p-4 border-4">
-            Video Conatiner
-        </div>
-        <div class="row-span-2 p-4 border-4">
-            Output COntainer
-        </div>
+            <div class=" row-span-2 box-content h-96 w-96 p-4 border-4">Video Conatiner</div>
+            <div class="row-span-2 box-content h-96 w-96 p-4 border-4">Output COntaine</div>   
     </div>
 
-    <div class="grid grid-rows-3 grid-flow-col gap-4">
-        <div class="box-content h-64 w-64 p-4 border-4">Video Conatiner</div>
-        <div class="box-content h-64 w-64 p-4 border-4">Output COntainer</div>
-    </div>
 </body>
 </html>
