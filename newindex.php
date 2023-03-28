5315f1a7610841642e6ebb4d69f714c0fbcc5c5a
vishalupadhyayrsm <vishalm.rsm@gmail.com>
2023-03-28 12:52:22 +0530

    data

--- newindex.php
+++ newindex.php
@@ -10,10 +10,11 @@
         <h2 class="text-center font-serif text-4xl font-black text-white">Face Rec</h2>
     </div>
    
-    <div class="grid grid-rows grid-flow-col gap-4">
-            <div class="row-span-2 box-content h-96 w-96 p-4 border-4">Video Conatiner</div>
-            <div class="row-span-2 box-content h-96 w-96 p-4 border-4">Output COntaine</div>   
+    <div class="">
+        <div class="w-full h-8 space-y-4 border-2 mb-1  sm:w-auto sm:border-2 sm:h-16 md:w-96 lg:w-32">Video Conatiner</div>
+        <div class="w-full h-8 space-y-4 border-2 mb-1  sm:w-auto sm:border-2 sm-h-16 md:w-96 lg:w-32">Output COntaine</div>   
     </div>
-
-</body>
+    <!-- sm:row-span-1  md:row-span-2 box-content  md:h-96 md:w-96  p-4 border-4
+    sm:h-12   sm:row-span-1  md:row-span-2 box-content  md:h-96 md:w-96  p-4 border-4  -->
+    </body>
 </html>
