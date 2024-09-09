e9cec7da8c96ad08f6e9e2dddba7cf4d7b9874f1
Vishal Kumar Upadhyay <46557542+vishalupadhyayrsm@users.noreply.github.com>
2024-09-09 16:20:09 +0530

    Data

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html>
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,41 +8,42 @@
   <script src="https://cdn.jsdelivr.net/npm/@ericblade/quagga2@1.2.6/dist/quagga.js"></script>
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body class="bg-slate-800 ">
-    <div class="w-full h-auto  mb-10 shadow-xl py-2">
-        <h2 class="text-center font-serif text-4xl font-black text-white">Scanner</h2>
+  <div class="w-full h-auto  mb-10 shadow-xl py-2">
+    <h2 class="text-center font-serif text-4xl font-black text-white">Scanner</h2>
+  </div>
+  <!-- this is video box container -->
+  <div class="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 lg:mx-20">
+    <div class="w-10/12 h-auto mx-9 space-y-4 mb-1 sm:w-full sm:h-auto sm:m-1 md:w-96  lg:w-11/12 lg:mb-0 xl:11/12">
+      <!-- <h2 class="text-center text-white font-serif text-4xl font-black">BarCode Detector</h2> -->
+      <video id="camera" class="border-2  rounded-md w-full sm:w-10/12 sm:mx-9 sm:my-10 lg:11/12 lg:h-96 "></video>
     </div>
-    <!-- this is video box container -->
-    <div class="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 lg:mx-20">
-        <div class="w-10/12 h-auto mx-9 space-y-4 mb-1 sm:w-full sm:h-auto sm:m-1 md:w-96  lg:w-11/12 lg:mb-0 xl:11/12">
-            <!-- <h2 class="text-center text-white font-serif text-4xl font-black">BarCode Detector</h2> -->
-            <video id="camera" class="border-2  rounded-md w-full sm:w-10/12 sm:mx-9 sm:my-10 lg:11/12 lg:h-96 "></video>
-        </div>
-       <!-- outpurt will be shown here -->
-        <div class="w-10/12 h-auto mx-9 space-y-4  mb-1 sm:w-full sm:h-auto sm:m-1 md:w-10/12 lg:w-11/12 lg:mb-0  xl:11/12">
-        <!-- <h2 class="text-center text-white font-serif text-4xl font-black">BarCode</h2> -->
-        <h2 class="text-justify text-white font-serif ">Bar Code:</h2>
-            <div class="h-11  border-2 rounded-md w-full sm:h-8  mx-1 my-1 lg:w-9/12  lg:h-14">
-              <h2 id="scandata" class="text-center text-white font-serif text-4xl font-black"></h2>
-            </div>
-            <div class="w-full rounded-md lg:w-9/12  lg:h-72">
-            <h2 class="text-justify text-white font-serif ">Capture Image:</h2>
-              <canvas id="canvas" class="w-full border-2  rounded-md h-full "></canvas>
-              <img id="photo" alt="no image" class="hidden lg:w-6/12">
-            </div>
-        </div>
-        <!-- <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-full text-sm px-1 py-2.5 text-center" id="btnScanner">Scan Barcode</button>
+    <!-- outpurt will be shown here -->
+    <div class="w-10/12 h-auto mx-9 space-y-4  mb-1 sm:w-full sm:h-auto sm:m-1 md:w-10/12 lg:w-11/12 lg:mb-0  xl:11/12">
+      <!-- <h2 class="text-center text-white font-serif text-4xl font-black">BarCode</h2> -->
+      <h2 class="text-justify text-white font-serif ">Bar Code:</h2>
+      <div class="h-11  border-2 rounded-md w-full sm:h-8  mx-1 my-1 lg:w-9/12  lg:h-14">
+        <h2 id="scandata" class="text-center text-white font-serif text-4xl font-black"></h2>
+      </div>
+      <div class="w-full rounded-md lg:w-9/12  lg:h-72">
+        <h2 class="text-justify text-white font-serif ">Capture Image:</h2>
+        <canvas id="canvas" class="w-full border-2  rounded-md h-full "></canvas>
+        <img id="photo" alt="no image" class="hidden lg:w-6/12">
+      </div>
+    </div>
+    <!-- <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-full text-sm px-1 py-2.5 text-center" id="btnScanner">Scan Barcode</button>
         <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-full text-sm px-1 py-2.5 text-center" id="btnCapture" onclick="captureVideo()">Capture Image</button> -->
-    </div><br>
-    <div class="mx-20 sm:w-2/3 sm:mx-64  md:mx-56 lg:mx-96 xl:mx-96">
-        <img src="image/bar.png"    class="float-left w-8 mx-2.5 sm:w-20 h-16 sm:mx-1 md:mx-3 lg:mx-4 2xl:mx-8" id="btnScanner"/>
-        <img src="image/camera.png" class="float-left w-8 mx-2.5 sm:w-20 h-16 sm:mx-1 md:mx-3 lg:mx-4 2xl:mx-8" id="btnCapture" onclick="captureImage()" />
-        <button type="button" class="float-left h-16 w-20 bg-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full 2xl:mx-8" id="btnSubmit">Submit</button>  
-    </div>
-    <!-- sm:row-span-1  md:row-span-2 box-content  md:h-96 md:w-96  p-4 border-4
+  </div><br>
+  <div class="mx-20 sm:w-2/3 sm:mx-64  md:mx-56 lg:mx-96 xl:mx-96">
+    <img src="image/bar.png" class="float-left w-8 mx-2.5 sm:w-20 h-16 sm:mx-1 md:mx-3 lg:mx-4 2xl:mx-8" id="btnScanner" />
+    <img src="image/camera.png" class="float-left w-8 mx-2.5 sm:w-20 h-16 sm:mx-1 md:mx-3 lg:mx-4 2xl:mx-8" id="btnCapture" onclick="captureImage()" />
+    <button type="button" class="float-left h-16 w-20 bg-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full 2xl:mx-8" id="btnSubmit">Submit</button>
+  </div>
+  <!-- sm:row-span-1  md:row-span-2 box-content  md:h-96 md:w-96  p-4 border-4
     sm:h-12   sm:row-span-1  md:row-span-2 box-content  md:h-96 md:w-96  p-4 border-4  -->
-    <script>
-     const video = document.getElementById('camera');
+  <script>
+    const video = document.getElementById('camera');
     var model, embeddings;
     const result = document.getElementById('result');
     const photo = document.getElementById('photo');
@@ -54,92 +56,94 @@
     btnScanner.addEventListener("click", scanbarcode);
     btnSubmit.addEventListener("click", make_predition);
 
-    navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => {
+    navigator.mediaDevices.getUserMedia({
+        video: true
+      })
+      .then(stream => {
         video.srcObject = stream;
         video.play();
       })
 
     // JavaScript
-    function scanbarcode(){
+    function scanbarcode() {
       document.getElementById("scandata").innerHTML = "";
-        // Initialize QuaggaJS to decode barcodes
-        Quagga.init({
+      // Initialize QuaggaJS to decode barcodes
+      Quagga.init({
         inputStream: {
-            name: "Live",
-            type: "LiveStream",
-            target: video
+          name: "Live",
+          type: "LiveStream",
+          target: video
         },
         decoder: {
-            readers: [
-                "code_128_reader"
-                // "ean_reader",
-                // "ean_8_reader",
-                // "code_39_reader",
-                // "code_39_vin_reader",
-                // "codabar_reader",
-                // "upc_reader",
-                // "upc_e_reader",
-                // "i2of5_reader",
-                // "2of5_reader",
-                // "code_93_reader",
-                // "code_32_reader"
-            ]  
+          readers: [
+            "code_128_reader"
+            // "ean_reader",
+            // "ean_8_reader",
+            // "code_39_reader",
+            // "code_39_vin_reader",
+            // "codabar_reader",
+            // "upc_reader",
+            // "upc_e_reader",
+            // "i2of5_reader",
+            // "2of5_reader",
+            // "code_93_reader",
+            // "code_32_reader"
+          ]
         }
-        }, (err) => {
+      }, (err) => {
         if (err) {
-            console.log(err);
-            return;
+          console.log(err);
+          return;
         }
         console.log('Initialization finished, Ready to start');
         Quagga.start();
-        })
-        Quagga.onDetected((val) => {
-            // console.log(val.codeResult)
-            var  scandata = val.codeResult
-            console.log(scandata);
-            document.getElementById("scandata").innerHTML = scandata.code;
-            Quagga.stop();
-        })
-    // 
-  };
-  
-  function captureImage() {
-    var canvas = document.getElementById('canvas');
-        var context = canvas.getContext('2d');  
-        width = video.videoWidth;
-        height = video.videoHeight;
-        canvas.setAttribute('width', width);
-        canvas.setAttribute('height', height);
-        // Capture the image into canvas from Webcam streaming Video element  
-        context.drawImage(video, 0, 0, width, height); 
-        var data = canvas.toDataURL('image/png');
-        photo.setAttribute('src', data); 
-  }
+      })
+      Quagga.onDetected((val) => {
+        // console.log(val.codeResult)
+        var scandata = val.codeResult
+        console.log(scandata);
+        document.getElementById("scandata").innerHTML = scandata.code;
+        Quagga.stop();
+      })
+      // 
+    };
 
-    async function init() {
-    model = await tf.loadGraphModel('https://generalai.in/facerec_js/model/model.json');
+    function captureImage() {
+      var canvas = document.getElementById('canvas');
+      var context = canvas.getContext('2d');
+      width = video.videoWidth;
+      height = video.videoHeight;
+      canvas.setAttribute('width', width);
+      canvas.setAttribute('height', height);
+      // Capture the image into canvas from Webcam streaming Video element  
+      context.drawImage(video, 0, 0, width, height);
+      var data = canvas.toDataURL('image/png');
+      photo.setAttribute('src', data);
     }
 
-    function pre_process(tfTensor){
-      tfTensor = tf.image.resizeBilinear(tfTensor, [160,160]).toFloat();
+    async function init() {
+      model = await tf.loadGraphModel('url');
+    }
+
+    function pre_process(tfTensor) {
+      tfTensor = tf.image.resizeBilinear(tfTensor, [160, 160]).toFloat();
       tfTensor = tfTensor.div(tf.scalar(255.0)).expandDims();
       return tfTensor;
     }
 
-    function make_predition(){
-        img = document.getElementById("photo");
-        let tfTensor = tf.browser.fromPixels(img);
-        tfTensor = pre_process(tfTensor);
-        img_embedding = model.execute([tfTensor]);
-        img_embedding = img_embedding.squeeze();
-        fetch('https://generalai.in/facerec_js/db/mean_embeddings.json')
+    function make_predition() {
+      img = document.getElementById("photo");
+      let tfTensor = tf.browser.fromPixels(img);
+      tfTensor = pre_process(tfTensor);
+      img_embedding = model.execute([tfTensor]);
+      img_embedding = img_embedding.squeeze();
+      fetch('url')
         .then(res => res.json())
         .then(embeddings => {
           label = [];
           sim_score = [];
 
-          for(let person in embeddings){
+          for (let person in embeddings) {
             let per_embedding = embeddings[person];
             per_embedding = tf.tensor1d(per_embedding);
             cos_sim = tf.abs(tf.metrics.cosineProximity(img_embedding, per_embedding)).dataSync()[0];
@@ -149,15 +153,15 @@
 
           label_idx = tf.argMax(tf.tensor1d(sim_score), -1).dataSync()[0];
           max_score = sim_score[label_idx]
-          if(max_score > thresh){
-            alert("Detected: "+label[label_idx]+" sim_score: "+max_score);
-          }
-          else{
+          if (max_score > thresh) {
+            alert("Detected: " + label[label_idx] + " sim_score: " + max_score);
+          } else {
             alert("Detected: unkown!");
           }
         })
     }
     init();
-    </script>
-    </body>
+  </script>
+</body>
+
 </html>
--- indexold.php
+++ indexold.php
@@ -1,28 +1,30 @@
 <!DOCTYPE html>
-<html> 
-    <head>
-        <meta name="viewport" content="width=device-width,initial-scale=1.0">
-        <link rel="stylesheet" href="css/index.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.2.0/dist/tf.min.js"> </script>
-        <script src="https://cdn.jsdelivr.net/npm/@ericblade/quagga2@1.2.6/dist/quagga.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-    </head>
-    <body>
-        <div class="container_box">
-            <div class="video_box">
-            <h2 class="barcode_txt">BarCode Detector</h2>
-            <video id="camera" class="video"></video>
-            </div>
-            <div class="output_box">
+<html>
 
-            </div>
-        </div>
-        <div class="row">
-        <i class="fa fa-camera" style="font-size:48px;color:blue"></i>
-        </div>
-        <script>
-         const video = document.getElementById('camera');
+<head>
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <link rel="stylesheet" href="css/index.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.2.0/dist/tf.min.js"> </script>
+  <script src="https://cdn.jsdelivr.net/npm/@ericblade/quagga2@1.2.6/dist/quagga.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+</head>
+
+<body>
+  <div class="container_box">
+    <div class="video_box">
+      <h2 class="barcode_txt">BarCode Detector</h2>
+      <video id="camera" class="video"></video>
+    </div>
+    <div class="output_box">
+
+    </div>
+  </div>
+  <div class="row">
+    <i class="fa fa-camera" style="font-size:48px;color:blue"></i>
+  </div>
+  <script>
+    const video = document.getElementById('camera');
     var model, embeddings;
     const result = document.getElementById('result');
     const photo = document.getElementById('photo');
@@ -35,92 +37,94 @@
     btnScanner.addEventListener("click", scanbarcode);
     btnSubmit.addEventListener("click", make_predition);
 
-    navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => {
+    navigator.mediaDevices.getUserMedia({
+        video: true
+      })
+      .then(stream => {
         video.srcObject = stream;
         video.play();
       })
 
     // JavaScript
-    function scanbarcode(){
+    function scanbarcode() {
       document.getElementById("scandata").innerHTML = "";
-        // Initialize QuaggaJS to decode barcodes
-        Quagga.init({
+      // Initialize QuaggaJS to decode barcodes
+      Quagga.init({
         inputStream: {
-            name: "Live",
-            type: "LiveStream",
-            target: video
+          name: "Live",
+          type: "LiveStream",
+          target: video
         },
         decoder: {
-            readers: [
-                "code_128_reader"
-                // "ean_reader",
-                // "ean_8_reader",
-                // "code_39_reader",
-                // "code_39_vin_reader",
-                // "codabar_reader",
-                // "upc_reader",
-                // "upc_e_reader",
-                // "i2of5_reader",
-                // "2of5_reader",
-                // "code_93_reader",
-                // "code_32_reader"
-            ]  
+          readers: [
+            "code_128_reader"
+            // "ean_reader",
+            // "ean_8_reader",
+            // "code_39_reader",
+            // "code_39_vin_reader",
+            // "codabar_reader",
+            // "upc_reader",
+            // "upc_e_reader",
+            // "i2of5_reader",
+            // "2of5_reader",
+            // "code_93_reader",
+            // "code_32_reader"
+          ]
         }
-        }, (err) => {
+      }, (err) => {
         if (err) {
-            console.log(err);
-            return;
+          console.log(err);
+          return;
         }
         console.log('Initialization finished, Ready to start');
         Quagga.start();
-        })
-        Quagga.onDetected((val) => {
-            // console.log(val.codeResult)
-            var  scandata = val.codeResult
-            console.log(scandata);
-            document.getElementById("scandata").innerHTML = scandata.code;
-            Quagga.stop();
-        })
-    // 
-  };
-  
-  function captureImage() {
-    var canvas = document.getElementById('canvas');
-        var context = canvas.getContext('2d');  
-        width = video.videoWidth;
-        height = video.videoHeight;
-        canvas.setAttribute('width', width);
-        canvas.setAttribute('height', height);
-        // Capture the image into canvas from Webcam streaming Video element  
-        context.drawImage(video, 0, 0, width, height); 
-        var data = canvas.toDataURL('image/png');
-        photo.setAttribute('src', data); 
-  }
+      })
+      Quagga.onDetected((val) => {
+        // console.log(val.codeResult)
+        var scandata = val.codeResult
+        console.log(scandata);
+        document.getElementById("scandata").innerHTML = scandata.code;
+        Quagga.stop();
+      })
+      // 
+    };
 
-    async function init() {
-    model = await tf.loadGraphModel('https://generalai.in/facerec_js/model/model.json');
+    function captureImage() {
+      var canvas = document.getElementById('canvas');
+      var context = canvas.getContext('2d');
+      width = video.videoWidth;
+      height = video.videoHeight;
+      canvas.setAttribute('width', width);
+      canvas.setAttribute('height', height);
+      // Capture the image into canvas from Webcam streaming Video element  
+      context.drawImage(video, 0, 0, width, height);
+      var data = canvas.toDataURL('image/png');
+      photo.setAttribute('src', data);
     }
 
-    function pre_process(tfTensor){
-      tfTensor = tf.image.resizeBilinear(tfTensor, [160,160]).toFloat();
+    async function init() {
+      model = await tf.loadGraphModel('url');
+    }
+
+    function pre_process(tfTensor) {
+      tfTensor = tf.image.resizeBilinear(tfTensor, [160, 160]).toFloat();
       tfTensor = tfTensor.div(tf.scalar(255.0)).expandDims();
       return tfTensor;
     }
 
-    function make_predition(){
-        img = document.getElementById("photo");
-        let tfTensor = tf.browser.fromPixels(img);
-        tfTensor = pre_process(tfTensor);
-        img_embedding = model.execute([tfTensor]);
-        img_embedding = img_embedding.squeeze();
-        fetch('https://generalai.in/facerec_js/db/mean_embeddings.json')
+    function make_predition() {
+      img = document.getElementById("photo");
+      let tfTensor = tf.browser.fromPixels(img);
+      tfTensor = pre_process(tfTensor);
+      img_embedding = model.execute([tfTensor]);
+      img_embedding = img_embedding.squeeze();
+      fetch('url')
         .then(res => res.json())
         .then(embeddings => {
           label = [];
           sim_score = [];
 
-          for(let person in embeddings){
+          for (let person in embeddings) {
             let per_embedding = embeddings[person];
             per_embedding = tf.tensor1d(per_embedding);
             cos_sim = tf.abs(tf.metrics.cosineProximity(img_embedding, per_embedding)).dataSync()[0];
@@ -130,16 +134,16 @@
 
           label_idx = tf.argMax(tf.tensor1d(sim_score), -1).dataSync()[0];
           max_score = sim_score[label_idx]
-          if(max_score > thresh){
-            alert("Detected: "+label[label_idx]+" sim_score: "+max_score);
-          }
-          else{
+          if (max_score > thresh) {
+            alert("Detected: " + label[label_idx] + " sim_score: " + max_score);
+          } else {
             alert("Detected: unkown!");
           }
         })
     }
-    
+
     init();
-        </script>   
-    </body>
+  </script>
+</body>
+
 </html>
